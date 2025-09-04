7d1bef22297c6ce1a34af7b20cd2417ae4bb7210
Paul Olabisi <18728756+paulonevrything@users.noreply.github.com>
2025-09-04 10:03:01 +0100

    initial commit

---
+++ src/main/java/com/paulonevrything/modulith_example/ModulithExampleApplication.java
@@ -0,0 +1,13 @@
+package com.paulonevrything.modulith_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulithExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulonevrything/modulith_example/ModulithExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulonevrything.modulith_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
